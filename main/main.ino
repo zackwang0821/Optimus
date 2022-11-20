d2c33b60406a0cfa10efa5cd6ac3a4276aa6fa8b
Zack <zackwang0821@gmail.com>
2022-11-20 19:20:48 +0800

    Add files for Arduino & controller

---
+++ main/main.ino
@@ -0,0 +1,19 @@
+#include<Servo.h>
+//Define the command of module
+#define READ_REG     0X00
+#define WRITE_REG    0x20
+#define RD_RX_PLOAD  0x61
+#define WR_TX_PLOAD  0xA0
+#define FLUSH_TX     0xE1
+#define FLUSH_RX     0xE2
+#define REUSE_TX_PL  0xE3
+#define NOP          0xFF
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
